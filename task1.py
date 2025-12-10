f018f9830e9f0d51a0a8305bcabe710253e67a33
ketya06 <udaryan.en@edu.spbstu.ru>
2025-12-10 18:36:40 +0300

    Лабораторная работа №2

---
+++ task1.py
@@ -0,0 +1,12 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+month = 0
+while money_capital + salary >= spend:
+    month += 1
+    money_capital -= (spend - salary)
+    spend *= (1 + increase)
+
+print("Количество месяцев, которое можно протянуть без долгов:", month)
